0048b79d574e8227cde70dc1a9d0600a9331bf30
Nicolas Nguyen <57681408+thqnn1999@users.noreply.github.com>
2020-04-05 14:23:16 -0400

    Update Cylinder_Area.s

--- inf1600_tp4/tp4_sources/Ex_1/Cylinder_Area.s
+++ inf1600_tp4/tp4_sources/Ex_1/Cylinder_Area.s
@@ -4,18 +4,7 @@ _ZNK8Cylinder7AreaAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
-        mov 8(%ebp), %ebx
-        mov 4(%ebx), %eax /* Pour la hauteur */
-        mov 8(%ebx), %edx /* Pour le rayon */
-        fld %eax
-        fld %edx
-        fmulp
-        fldpi
-        fmulp  /*Multiplication avec Pi */
-        mov %eax, 2 /* Apparition de la valeur 2 */
-        fld %eax 
-        fmulp
-        fstp %eax /* La valeur de l'aire est dans eax */
+        
         
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
