676ce59924977beca5e573e011056d0826122c58
Nicolas Nguyen <57681408+thqnn1999@users.noreply.github.com>
2020-04-05 15:30:30 -0400

    Update Cylinder_Area.s

--- inf1600_tp4/tp4_sources/Ex_1/Cylinder_Area.s
+++ inf1600_tp4/tp4_sources/Ex_1/Cylinder_Area.s
@@ -4,7 +4,22 @@ _ZNK8Cylinder7AreaAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
+        mov 8(%ebp), %ebx
+        mov (%ebx), %eax
+        mov 32(%eax), %edx
+        push %ebx
+        call *%edx /* Base area */
+        fld %eax
         
+        mov 8(%ebp),%ebx
+        mov (%ebx), %eax
+        mov 40(%eax), %edx
+        push %ebx
+        call *%edx
+        fld %eax /* Lat. area */
         
+        faddp
+        fstp %eax
+    
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
