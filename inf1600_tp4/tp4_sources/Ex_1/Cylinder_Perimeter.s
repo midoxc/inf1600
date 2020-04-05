cb329044b81538c315ea1d7e62787fe1715b146e
Nicolas Nguyen <57681408+thqnn1999@users.noreply.github.com>
2020-04-05 14:32:41 -0400

    Update Cylinder_Perimeter.s

--- inf1600_tp4/tp4_sources/Ex_1/Cylinder_Perimeter.s
+++ inf1600_tp4/tp4_sources/Ex_1/Cylinder_Perimeter.s
@@ -6,7 +6,22 @@ _ZNK8Cylinder12PerimeterAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
-        /* Write your solution here */
+        mov 8(%ebp), %ebx
+        mov 4(%ebx), %eax /* Pour la hauteur */
+        mov 8(%ebx), %edx /* Pour le Rayon */
+        fld %eax
+        fld %eax
+        faddp /* La longueur totale dans %eax*/
+        fld %edx
+        fldpi
+        fmulp
+        faddp /* 2*Pi *r dans %edx */
+        faddp /* La largeur totale de %edx */
+        fld %eax
+        faddp
+        fstp %eax /* Perimetre total dans %eax */
+        
+        
         
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
