75897e6cc57dfb0da4e1065c7cf214bb5864148f
Nicolas Nguyen <57681408+thqnn1999@users.noreply.github.com>
2020-04-05 14:23:34 -0400

    Update Cylinder_LateralArea.s

--- inf1600_tp4/tp4_sources/Ex_1/Cylinder_LateralArea.s
+++ inf1600_tp4/tp4_sources/Ex_1/Cylinder_LateralArea.s
@@ -4,7 +4,18 @@ _ZNK8Cylinder14LateralAreaAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
-        /* Write your solution here */
+        mov 8(%ebp), %ebx
+        mov 4(%ebx), %eax /* Pour la hauteur */
+        mov 8(%ebx), %edx /* Pour le rayon */
+        fld %eax
+        fld %edx
+        fmulp
+        fldpi
+        fmulp  /*Multiplication avec Pi */
+        mov %eax, 2 /* Apparition de la valeur 2 */
+        fld %eax 
+        fmulp   /* La valeur de l'aire est dans eax (2Pirh) */
+       
         
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
