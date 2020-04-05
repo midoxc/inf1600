ae0a9bebccfda35e7c450c7c55432a0c75218bce
Thien Quan Nicolas Nguyen <thnguam@l4712-14.info.polymtl.ca>
2020-04-05 15:50:50 -0400

    Fix dans Cylinde_Perimeter

--- inf1600_tp4/tp4_sources/Ex_1/Cylinder_Perimeter.s
+++ inf1600_tp4/tp4_sources/Ex_1/Cylinder_Perimeter.s
@@ -5,7 +5,8 @@ factor: .float 2.0 /* use this to mult by two */
 _ZNK8Cylinder12PerimeterAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
-
+        
+        mov 8(%ebp), %ebx
         mov 4(%ebx), %eax /* Pour la Rayon */
         mov 8(%ebx), %edx /* Pour le Hauteur */
         fld %eax
